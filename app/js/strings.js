cb5d5271282655f079426ffbe09a2ab2189491d8
John Lombardo <john@lombardos.org>
2014-02-07 19:12:03 -0800

    love / hate strings

---
+++ app/js/strings.js
@@ -0,0 +1,18 @@
+var hate = [
+    "Doomed",
+    "Not a chance",
+    "Destined for failure",
+    "Next WebVan",
+    "Next Pets.com",
+    "Really???"
+];
+
+var love = [
+    "Next Facebook",
+    "Next Pinterest",
+    "Next Dropbox",
+    "Next AirBNB",
+    "This. Changes. Everything.",
+    "They stole my idea.",
+    "Why didn't I think of this"
+];
